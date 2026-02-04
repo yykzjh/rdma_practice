fe8dc7f4ee7d1e4f93a3f56ef6aac525e5be32d0
yiyin.zjh <yiyin.zjh@alibaba-inc.com>
2026-02-04 11:54:36 +0800

    feat: implement pingpong library

---
+++ libibverbs/pingpong.cpp
@@ -0,0 +1,76 @@
+#include "include/pingpong.h"
+#include <endian.h>
+#include <iostream>
+#include <string>
+#include <array>
+#include <charconv>
+
+enum ibv_mtu pp_mtu_to_enum(int mtu)
+{
+	switch (mtu) {
+	case 256:
+		return IBV_MTU_256;
+	case 512:
+		return IBV_MTU_512;
+	case 1024:
+		return IBV_MTU_1024;
+	case 2048:
+		return IBV_MTU_2048;
+	case 4096:
+		return IBV_MTU_4096;
+	default:
+		return static_cast<ibv_mtu>(0);
+	}
+}
+
+int pp_get_port_info(struct ibv_context *context, int port,
+		     struct ibv_port_attr *attr)
+{
+	return ibv_query_port(context, port, attr);
+}
+
+void wire_gid_to_gid(std::string &wgid, union ibv_gid *gid)
+{
+	if (!gid) {
+		throw std::invalid_argument("gid is null");
+	}
+	if (wgid.size() < 32) {
+		throw std::invalid_argument(
+			"wgid must have at least 32 hex characters");
+	}
+
+	std::array<std::uint32_t, 4> tmp_gid{};
+
+	for (std::size_t i = 0; i < 4; ++i) {
+		const char *first = wgid.data() + i * 8;
+		const char *last = first + 8;
+
+		std::uint32_t v32_be = 0;
+		const auto res = std::from_chars(first, last, v32_be, 16);
+		if (res.ec != std::errc{} || res.ptr != last) {
+			throw std::invalid_argument(
+				"wgid contains invalid hex characters");
+		}
+		tmp_gid[i] = be32toh(v32_be);
+	}
+
+	memcpy(gid, tmp_gid.data(), sizeof(*gid));
+}
+
+void gid_to_wire_gid(const union ibv_gid *gid, std::string &wgid)
+{
+	if (!gid) {
+		wgid.clear();
+		return;
+	}
+
+	std::array<std::uint32_t, 4> tmp{};
+	memcpy(tmp.data(), gid, sizeof(tmp));
+
+	wgid.resize(33);
+
+	for (std::size_t i = 0; i < 4; ++i) {
+		std::uint32_t v = htobe32(tmp[i]);
+		std::snprintf(&wgid[i * 8], 9, "%08x", v);
+	}
+}
